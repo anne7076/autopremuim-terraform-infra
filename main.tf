cea2127cce36d8b988aabfdfcc43f492a7c2c3a5
ANNE Mahamadou <anne.mahamadou.ensao@ump.ac.ma>
2026-07-22 23:01:46 +0100

    improve Terraform configuration, move form localstack to aws - first functionnable infra

--- main.tf
+++ main.tf
@@ -7,41 +7,33 @@ terraform {
 }
 
 provider "aws" {
-  profile = "localstack"
+  profile = "default"
   region  = "us-east-1"
 }
 
-# network :
-resource "aws_vpc" "local_vpc" {
-  cidr_block = "172.16.0.0/16"
-  tags = {
-    Name = "local_vpc"
-  }
+# VPC
+data "aws_availability_zones" "zones" {
+  state = "available"
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.local_vpc.id
-  cidr_block = "172.16.1.0/24"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.0"
 
-  tags = {
-    Name = "public_subnet"
-  }
-}
-
-resource "aws_subnet" "db_subnet" {
-  for_each   = toset([1, 2])
-  vpc_id     = aws_vpc.local_vpc.id
-  cidr_block = "172.16.10.0/24"
-
-  tags = {
-    "name" = "private_subnet_${each.value}"
-  }
+  name                    = "local"
+  cidr                    = "10.0.0.0/16"
+  azs                     = data.aws_availability_zones.zones.names
+  public_subnets          = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets         = ["10.0.10.0/24", "10.0.20.0/24"]
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
 }
 
 # S3 bucket :
 resource "aws_s3_bucket" "terraform_bucket" {
-  bucket           = "test"
-  bucket_namespace = "global"
+  bucket_prefix = "terraform-bucket-"
+  #   bucket_namespace = "global"
 
   tags = {
     Name = "terraform_bucket"
@@ -50,19 +42,44 @@ resource "aws_s3_bucket" "terraform_bucket" {
 
 # Database :
 resource "aws_db_instance" "mysql_database" {
-  instance_class      = "db.t3.micro"
-  db_name             = "database"
-  engine              = "mysql"
-  engine_version      = "8.0"
+  instance_class = "db.t3.micro"
+  db_name        = "terraform_database"
+  engine         = "mysql"
+  engine_version = "8.0"
+
   username            = var.database_username
-  password            = var.database_username
+  password            = var.database_password
   allocated_storage   = 10
   skip_final_snapshot = true
-  #   db_subnet_group_name = aws_db_subnet_group.db_subnet.name
-}
 
-resource "aws_db_subnet_group" "db_subnet" {
-  subnet_ids = [aws_subnet.db_subnet.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids = [aws_security_group.db_security.id]
+}
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "db_subnet_group"
+  subnet_ids = module.vpc.private_subnets
+
+}
+resource "aws_security_group" "db_security" {
+  name_prefix = "db_security_"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "db_security_rule" {
+  ip_protocol                  = "tcp"
+  security_group_id            = aws_security_group.db_security.id
+  from_port                    = aws_db_instance.mysql_database.port
+  to_port                      = aws_db_instance.mysql_database.port
+  referenced_security_group_id = aws_security_group.ec2_security.id
 }
 
 # EC2 Instance :
@@ -78,43 +95,84 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_instance" "ec2_instance" {
-  count           = 2
-  ami             = data.aws_ami.amazon_linux.id
-  instance_type   = var.small_machine_type
-  subnet_id       = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.public_access.name]
-  cpu_options {
-    core_count       = 2
-    threads_per_core = 2
-  }
+  #   for_each               = module.vpc.public_subnets
+  count                  = 2
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.small_machine_type
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.ec2_security.id]
+
+  user_data = templatefile("${path.module}/code.sh", {
+    db_host     = aws_db_instance.mysql_database.address
+    db_user     = aws_db_instance.mysql_database.username
+    db_password = aws_db_instance.mysql_database.password
+    db_name     = aws_db_instance.mysql_database.db_name
+  })
+  user_data_replace_on_change = true
   tags = {
     Name = "ec2_instance_${count.index}"
   }
 }
 
-resource "aws_security_group" "public_access" {
-  name = "public_access"
+resource "aws_security_group" "ec2_security" {
+  name_prefix = "ec2_security_"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allw all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_security_group_rule" "allow_http" {
-  type              = "ingress"
-  security_group_id = aws_security_group.public_access.id
-  protocol          = "tcp"
-  from_port         = 80
-  to_port           = 80
-  cidr_blocks       = ["0.0.0.0/0"]
+resource "aws_vpc_security_group_ingress_rule" "ec2_security_rule" {
+  security_group_id            = aws_security_group.ec2_security.id
+  ip_protocol                  = "tcp"
+  from_port                    = var.application_port
+  to_port                      = var.application_port
+  referenced_security_group_id = aws_security_group.lb_security.id
 }
 
 # Load Balancer :
 resource "aws_lb" "load_balancer" {
+  name               = "test-lb"
   load_balancer_type = "application"
-  subnets            = [aws_subnet.public_subnet.id]
+  subnets            = module.vpc.public_subnets
+  security_groups    = [aws_security_group.lb_security.id]
+}
+
+resource "aws_security_group" "lb_security" {
+  name_prefix = "lb_security_"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb_security_rule" {
+  security_group_id = aws_security_group.lb_security.id
+  ip_protocol       = "tcp"
+  from_port         = var.internet_port
+  to_port           = var.internet_port
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_lb_listener" "lb_listener" {
+  # le port d'ecoute public
   load_balancer_arn = aws_lb.load_balancer.arn
   protocol          = "HTTP"
-  port              = 80
+  port              = var.internet_port
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ec2_group.arn
@@ -122,19 +180,22 @@ resource "aws_lb_listener" "lb_listener" {
 }
 
 resource "aws_lb_target_group" "ec2_group" {
-  port     = 80
+  port     = var.application_port
   protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
   health_check {
-    path     = "/health"
-    interval = 5
-    timeout  = 10
-
+    path                = "/health"
+    interval            = 30
+    timeout             = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
   }
 }
 
 resource "aws_lb_target_group_attachment" "lb_attachement" {
-  for_each         = aws_instance.ec2_instance
+  count            = 2
   target_group_arn = aws_lb_target_group.ec2_group.arn
-  target_id        = each.value.id
-  port             = 80
+  target_id        = aws_instance.ec2_instance[count.index].id
+  port             = var.application_port
 }
+
--- variables-outputs.tf
+++ variables-outputs.tf
@@ -4,16 +4,7 @@
 variable "small_machine_type" {
   type    = string
   default = "t3.micro"
-  #   validation {
-  #     condition     = var.small_machine_type in ["t2.micro", "t3.micro"]
-  #     error_message = "accepted machine type are ...."
-  #   }
 }
-variable "big_machine_type" {
-  type    = string
-  default = "...."
-}
-
 variable "database_username" {
   type      = string
   sensitive = true
@@ -24,10 +15,33 @@ variable "database_password" {
   sensitive = true
   default   = "changeme"
 }
+variable "application_port" {
+  type        = number
+  default     = 5000
+  description = "the port on which the application is running on"
+}
+variable "internet_port" {
+  type        = number
+  default     = 80
+  description = "the port for the incoming tcp traffic, 80/443"
+}
 
 # ============================================
 #  OUTPUTS
 # ============================================
-output "db_ip_adress" {
-  value = aws_db_instance.mysql_database.address
+output "db_hostname" {
+  value       = aws_db_instance.mysql_database.address
+  description = "the database dns name"
+}
+output "db_port" {
+  value = aws_db_instance.mysql_database.port
+}
+
+output "lb_hostname" {
+  value       = aws_lb.load_balancer.dns_name
+  description = "the load balancer dns name"
+}
+
+output "bucket_id" {
+  value = aws_s3_bucket.terraform_bucket.id
 }
