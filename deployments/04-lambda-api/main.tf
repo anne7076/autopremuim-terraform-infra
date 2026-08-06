54e479210bb5d7d34ff5ebce956af5f7beecd6ef
ANNE Mahamadou <anne.mahamadou.ensao@ump.ac.ma>
2026-08-06 18:35:07 +0100

    chore: add EKS and Lambda deployment templates

---
+++ deployments/04-lambda-api/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket       = "terraform-state-bucket-f8080b23866255cb548f14e56b"
+    key          = "learning/lambda-api/terraform.tfstate"
+    use_lockfile = true
+    encrypt      = true
+    region       = "us-east-1"
+  }
+}
+
+locals {
+  is_localstack = terraform.workspace == "localstack"
+}
